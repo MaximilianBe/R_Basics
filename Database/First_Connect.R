f9dc6c17217485f2e92b61d7c7b0908f2321247e
Max B <mab8134@thi.de>
2025-09-10 11:08:38 +0200

    failed postgres_connect

--- Database/First_Connect.R
+++ Database/First_Connect.R
@@ -1,15 +1,20 @@
 library(DBI)
 library(RPostgres)
+library(Billomatics)
+library(tibble)
+library(dplyr)
+library(shiny)
 
-con <- dbConnect(
-  RPostgres::Postgres(),
-  dbname = "postgres",
-  host = "localhost",
-  port = 5432,
-  user = "postgres",
-  password = getPass::getPass(),
-  client_encoding = "utf8"
-)
+con <- Billomatics::postgres_connect(
+  needed_tables = c("raw.billomat_invoices"),
 
-print(dbListTables(con))
-print(dbIsValid(con))
+  postgres_keys = keys$postgresql,
+  local_password_is_product = TRUE,
+  update_local_tables = TRUE,
+  ssh_key_path = "C:/Users/maxep/.ssh/id_rsa"
+) # Falls anders)
+
+onStop(function() {
+  poolClose(con)
+}) # pool wird automatisch nach App-Laufzeit geschlossen
+tasks_comments <- tbl(con, I("raw.billomat_invoices")) %>% collect()
